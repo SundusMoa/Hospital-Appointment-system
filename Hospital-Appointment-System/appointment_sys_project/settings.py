1203d5a63798239f90f773d37cb4f5d9a4ad18a6
SundusMoa <sundusmoa10@gmail.com>
2025-10-10 22:13:42 +0300

    Building the models Doctor , User and Appointment which is the class is the table and objects it the row , then always after add model or modify model we will migrate tabels into database

--- Hospital-Appointment-System/appointment_sys_project/settings.py
+++ Hospital-Appointment-System/appointment_sys_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'appointment_sys_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Hospital_Appointment_system',  # Name of your PostgreSQL database
+        'USER': 'postgres',
+        'PASSWORD':'sa123456789',
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
 
--- Hospital-Appointment-System/main_app/models.py
+++ Hospital-Appointment-System/main_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Doctor (models.Model): # class is the table and objects is the rows
+    doctor_id = models.BigAutoField (primary_key=True)
+    doctor_name = models.CharField(max_length=50 , null=False) # this field CANNOT be null
+    doctor_specialization = models.CharField(max_length=50 , null=False)# this field CANNOT be null
+
+    def __self__(self):
+        return self.doctor_name
+
+class User (models.Model):
+    user_id=models.BigAutoField (primary_key=True)
+    user_name= models.CharField(max_length=50 , null=False) # this field CANNOT be null
+    user_email=models.EmailField(max_length=254 , null=False) # this field CANNOT
+    user_password=models.CharField(max_length=50 , null=False) # this field CANNOT be null  
+
+    def __self__(self):
+        return self.user_name 
+
+class Appointments (models.Model):
+    patient_name = models.CharField(max_length=50 , null=False) # this field CANNOT be null
+    date = models.DateField(null=False) # this field CANNOT be null
+    time = models.TimeField(null=False) # this field CANNOT be null
+
+    # the relation is one to many (one doctor can have many appointments & one user can have many appointments)
+    doctor = models.ForeignKey(Doctor , related_name='appointments', on_delete=models.CASCADE,null=True) # this field can be null
+    user = models.ForeignKey(User, related_name='appointments',on_delete=models.CASCADE,null=True) # this field can be null
+
+    def __self__(self):
+        return self.patient_name
